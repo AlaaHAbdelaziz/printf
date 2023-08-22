2590d9c7f3a7536b782e746bac6fd294295e3559
AlaaHAbdelaziz <alaa.h.abdelaziz@gmail.com>
2023-08-22 12:56:56 -0700

    string_fields.c file has been created

---
+++ string_fields.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_precision - gets percision from format string
+ * @poin: format string
+ * @params: parametrs struct
+ * @list: arguments pointer
+ * Return: new pointer
+ */
+
+char *get_precision(char *poin, params_t *params, va_list list)
+{
+int d = 0;
+if (*poin != '.')
+return (poin);
+poin++;
+if (*poin == '*')
+{
+d = va_arg(list, int);
+poin++;
+}
+else
+{
+while (_isdigit(*poin))
+d = d * 10 + (*poin++ - '0');
+}
+params->percision = d;
+return (poin);
+}
